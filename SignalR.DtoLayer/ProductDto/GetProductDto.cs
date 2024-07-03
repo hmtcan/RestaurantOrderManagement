d5524c6c718e2e2fb8b614fbdf6a2deb7daab772
Hamit Can Sayılgan <hamitcansayilgan@gmail.com>
2024-07-03 12:38:30 +0300

    Created DTO entity properties in the DTO layer. Installed AutoMapper and Dependency Injection packages via NuGet in the SignalR API.

--- SignalR.DtoLayer/ProductDto/GetProductDto.cs
+++ SignalR.DtoLayer/ProductDto/GetProductDto.cs
@@ -1,6 +1,12 @@
-﻿namespace SignalR.EntityLayer.Entities
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SignalR.DtoLayer.ProductDto
 {
-    public class Product
+    public class GetProductDto
     {
         public int ProductID { get; set; }
         public string ProductName { get; set; }
