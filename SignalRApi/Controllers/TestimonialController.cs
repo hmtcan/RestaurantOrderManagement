e01796f71178e223890c16ed2cca68eb0eb67221
Hamit Can Sayılgan <hamitcansayilgan@gmail.com>
2024-07-20 01:25:07 +0300

    Implemented CRUD operations in Feature, Social Media and Testimonial Controller and created Feature, Social Media and Testimonial create and update pages.

--- SignalRApi/Controllers/TestimonialController.cs
+++ SignalRApi/Controllers/TestimonialController.cs
@@ -39,14 +39,14 @@ namespace SignalRApi.Controllers
             });
             return Ok("Müşteri Yorum bilgisi başarıyla eklendi!");
         }
-        [HttpDelete]
+        [HttpDelete("{id}")]
         public IActionResult DeleteTestimonial(int id)
         {
             var value = _testimonialService.TGetByID(id);
             _testimonialService.TDelete(value);
             return Ok("Müşteri Yorum bilgisi başarıyla silindi!");
         }
-        [HttpGet("GetTestimonial")]
+        [HttpGet("{id}")]
         public IActionResult GetTestimonial(int id)
         {
             var value = _testimonialService.TGetByID(id);
